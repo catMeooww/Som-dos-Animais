d0df286b824310c7df5c2e12834b6cd812d7de0c
catMeooww <110690475+catMeooww@users.noreply.github.com>
2023-03-28 15:52:21 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 //https://teachablemachine.withgoogle.com/models/RSRXxuKrf/model.json
+function Classification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/RSRXxuKrf/model.json",modelReady)
+}
+function modelReady(){
+    classifier.classify(gotResult)
+}
+function gotResult(error,results){
+    if (error){
+     console.error(error)
+    }else{
+        console.log(results)
+    }
+}
