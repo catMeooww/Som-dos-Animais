4391385cd841545274969d4d578f1cb3f30b8c5d
catMeooww <110690475+catMeooww@users.noreply.github.com>
2023-04-04 16:08:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 //https://teachablemachine.withgoogle.com/models/RSRXxuKrf/model.json
+var animalImg = document.getElementById("Animal");
 function Classification(){
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/RSRXxuKrf/model.json",modelReady)
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/RSRXxuKrf/model.json",modelReady);
 }
 function modelReady(){
-    classifier.classify(gotResult)
+    classifier.classify(gotResult);
 }
 function gotResult(error,results){
     if (error){
-     console.error(error)
+     console.error(error);
     }else{
-        console.log(results)
+        console.log(results);
+        document.getElementById("ListenAnimal").innerHTML = "Posso ouvir: "+results[0].label;
+        document.getElementById("ListenConfidence").innerHTML = "Precisão- "+(results[0].confidence * 100).toFixed(2) + "%";
+        if (results[0].label == "Ovelha"){
+            animalImg.src = "ovelha.jpg";
+        }else if (results[0].label == "Gato"){
+            animalImg.src = "gato.webp";
+        }else if (results[0].label == "Vaca"){
+            animalImg.src = "vaca.jpg";
+        }else if (results[0].label == "Cachorro"){
+            animalImg.src = "cachorro.jpg";
+        }else{
+            animalImg.src = "listen.gif";
+        }
     }
 }
